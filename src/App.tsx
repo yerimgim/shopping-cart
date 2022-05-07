e3f2252a6ed37993293f7bff3449a4d9077eb765
imyr <imyrgim92@gmail.com>
2022-05-08 04:30:16 +0900

    chore: Add eslint, prettier

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App() {
-  return (
-  <h1>shopping-cart</h1>
-  );
-}
-
-export default App;
---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>shopping-cart</h1>;
+};
+
+export default App;
